d9e1c5f1bbfbb308cff69f1784147d11783d709a
Juan Ochoteco <jochotecoa@gmail.com>
2022-02-13 01:00:25 +0100

    13-Feb-22

---
+++ scripts/model_testing/testing_model_rf_21.R
@@ -0,0 +1,128 @@
+source('scripts/functions/functions_JOA.R')
+forceLibrary(c('mlbench', 'caret', 'doMC', 'dplyr', 'RANN'))
+
+train_mod_path = '/ngs-data-2/analysis/juan/autosign/trained_models/apap_21vs21'
+conf_matr_path = 'output/confusion_matrices/apap_21vs21'
+
+model_rf = readRDS(paste0(train_mod_path, '/rf/original.rds'))
+
+
+# Testing 9vs9 data -------------------------------------------------------
+
+
+test_data_9_path = 'data/apap_hecatos/deseq2_dataset_9vs9.rds' 
+test_data_9 = test_data_9_path %>% readRDS()
+
+colnames(test_data_9) = colnames(test_data_9) %>% 
+  make.names()
+
+# Convert all logical variables to numerical
+test_data_9[sapply(test_data_9, is.logical)] = 
+  test_data_9[sapply(test_data_9, is.logical)] %>% 
+  sapply(as.numeric)
+
+table(model_rf$coefnames %in% colnames(test_data_9))
+
+test_data_9_ori = test_data_9
+
+test_data_9 = 
+  test_data_9[, colnames(test_data_9) %in% model_rf$coefnames]
+
+test_data_9$significance = test_data_9_ori$significance
+
+
+
+obsVSpred <- data.frame(actual = test_data_9$significance,
+                    predict(model_rf, newdata = test_data_9))
+obsVSpred$predict = obsVSpred[, 2] %>% as.factor()
+
+cm_test_data_9 <- confusionMatrix(obsVSpred$predict, test_data_9$significance)
+
+cm_test_data_9$byClass[, 1:4] %>% naToZero %>% rowMeans()
+
+if (!dir.exists(paste0(conf_matr_path, '/rf/'))) {
+  dir.create(paste0(conf_matr_path, '/rf/'), recursive = T)
+}
+
+cm_test_data_9 %>% saveRDS(paste0(conf_matr_path, '/rf/test_data_9.rds'))
+
+
+# Testing 3vs3 data -------------------------------------------------------
+
+
+test_data_3_path = 'data/apap_hecatos/deseq2_dataset_3vs3.rds' 
+test_data_3 = test_data_3_path %>% readRDS()
+
+colnames(test_data_3) = colnames(test_data_3) %>% 
+  make.names()
+
+# Convert all logical variables to numerical
+test_data_3[sapply(test_data_3, is.logical)] = 
+  test_data_3[sapply(test_data_3, is.logical)] %>% 
+  sapply(as.numeric)
+
+table(model_rf$coefnames %in% colnames(test_data_3))
+
+test_data_3_ori = test_data_3
+
+test_data_3 = 
+  test_data_3[, colnames(test_data_3) %in% model_rf$coefnames]
+
+test_data_3$significance = test_data_3_ori$significance
+
+
+
+obsVSpred <- data.frame(actual = test_data_3$significance,
+                        predict(model_rf, newdata = test_data_3))
+obsVSpred$predict = obsVSpred[, 2] %>% as.factor()
+
+cm_test_data_3 <- confusionMatrix(obsVSpred$predict, test_data_3$significance)
+
+cm_test_data_3$byClass[, 1:4] %>% naToZero %>% rowMeans()
+
+if (!dir.exists(paste0(conf_matr_path, '/rf/'))) {
+  dir.create(paste0(conf_matr_path, '/rf/'), recursive = T)
+}
+
+cm_test_data_3 %>% saveRDS(paste0(conf_matr_path, '/rf/test_data_3.rds'))
+
+
+# Testing 3_9_21 data -------------------------------------------------------
+
+
+test_data_3_9_21_path = 'data/apap_hecatos/deseq2_dataset_3_9_21.rds' 
+test_data_3_9_21 = test_data_3_9_21_path %>% readRDS()
+
+colnames(test_data_3_9_21) = colnames(test_data_3_9_21) %>% 
+  make.names()
+
+# Convert all logical variables to numerical
+test_data_3_9_21[sapply(test_data_3_9_21, is.logical)] = 
+  test_data_3_9_21[sapply(test_data_3_9_21, is.logical)] %>% 
+  sapply(as.numeric)
+
+table(model_rf$coefnames %in% colnames(test_data_3_9_21))
+
+test_data_3_9_21_ori = test_data_3_9_21
+
+test_data_3_9_21 = 
+  test_data_3_9_21[, colnames(test_data_3_9_21) %in% model_rf$coefnames]
+
+test_data_3_9_21$significance = test_data_3_9_21_ori$significance
+
+
+
+obsVSpred <- data.frame(actual = test_data_3_9_21$significance,
+                        predict(model_rf, newdata = test_data_3_9_21))
+obsVSpred$predict = obsVSpred[, 2] %>% as.factor()
+
+cm_test_data_3_9_21 <- confusionMatrix(obsVSpred$predict, test_data_3_9_21$significance)
+
+cm_test_data_3_9_21$byClass[, 1:4] %>% naToZero %>% rowMeans()
+
+if (!dir.exists(paste0(conf_matr_path, '/rf/'))) {
+  dir.create(paste0(conf_matr_path, '/rf/'), recursive = T)
+}
+
+cm_test_data_3_9_21 %>% saveRDS(paste0(conf_matr_path, '/rf/test_data_3_9_21.rds'))
+
