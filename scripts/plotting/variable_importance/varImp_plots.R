8be0ebd219904194007a3cba276b97919f7aabd0
Juan Ochoteco <jochotecoa@gmail.com>
2022-03-30 20:00:14 +0200

    30-Mar-22

--- scripts/plotting/variable_importance/varImp_plots.R
+++ scripts/plotting/variable_importance/varImp_plots.R
@@ -1,3 +1,7 @@
+
+# 3vs3 --------------------------------------------------------------------
+
+
 source('scripts/functions/functions_JOA.R')
 forceLibrary(c('mlbench', 'caret', 'doMC', 'dplyr', 'RANN', 'magrittr'))
 
@@ -23,5 +27,41 @@ tiff("output/plots/variable_importance/treebag_3vs3.tiff", units="in", width=10,
 par(mar=c(4,14,4,8))
 
 plot_varimp_3 %>% plot()
+
+dev.off()
+
+
+
+# 9vs9 --------------------------------------------------------------------
+
+source('scripts/functions/functions_JOA.R')
+forceLibrary(c('mlbench', 'caret', 'doMC', 'dplyr', 'RANN'))
+
+train_mod_path = '/ngs-data-2/analysis/juan/autosign/trained_models/apap_9vs9'
+conf_matr_path = 'output/confusion_matrices/apap_9vs9'
+
+model_sparseLDA = readRDS(paste0(train_mod_path, '/sparseLDA/original.rds'))
+
+
+test_data_9_path = 'data/apap_hecatos/deseq2_dataset_9vs9.rds' 
+test_data_9 = test_data_9_path %>% readRDS()
+
+plot_varimp_9 = model_sparseLDA %>% 
+  varImp %>% 
+  plot()
+
+# Above is written with a 'b', not 'avove' 
+plot_varimp_9$y.limits %<>% 
+  gsub(pattern = 'avo', replacement = 'abo')
+
+if (!dir.exists('output/plots/variable_importance/')) {
+  dir.create('output/plots/variable_importance/', recursive = T)
+}
+
+
+tiff("output/plots/variable_importance/sparseLDA_9vs9.tiff", units="in", width=10, height=20, res=300)
+par(mar=c(4,14,4,8))
+
+plot_varimp_9 %>% plot()
 
 dev.off()
